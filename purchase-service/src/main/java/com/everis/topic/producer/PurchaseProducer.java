c480eb58c8a3911a502d7abf2402c54eb4284f48
Carlos Alberto Palacios Aguado <cpalacag@emeal.nttdata.com>
2022-09-23 17:46:57 -0500

    Actualizaciones de properties

--- purchase-service/src/main/java/com/everis/topic/producer/PurchaseProducer.java
+++ purchase-service/src/main/java/com/everis/topic/producer/PurchaseProducer.java
@@ -2,21 +2,23 @@ package com.everis.topic.producer;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
 
 import com.everis.model.Purchase;
 
+@Component
 public class PurchaseProducer {
-	
+
 	@Autowired
 	private KafkaTemplate<String, Object> kafkaTemplate;
 
-	  private String createdPurchaseTopic = "created-purchase-topic";
+	private String createdPurchaseTopic = "created-purchase-topic";
 
-	  /** Envia datos del purchase al topico. */
-	  public void sendCreatePurchase(Purchase purchase) {
+	/** Envia datos del purchase al topico. */
+	public void sendCreatePurchase(Purchase purchase) {
 
-	    kafkaTemplate.send(createdPurchaseTopic, purchase);
+		kafkaTemplate.send(createdPurchaseTopic, purchase);
 
-	  }
+	}
 
 }
